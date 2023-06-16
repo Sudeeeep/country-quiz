88ea623ca1a9f01ce9731fe6a914b5dab73dafdd
Sudeeeep <sudeepn15@gmail.com>
2023-06-16 19:03:33 +0530

    feat: add toggle quiz functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { Home } from "./components/Home";
+import { CapitalsQuiz } from "./components/CapitalsQuiz";
+import { FlagsQuiz } from "./components/FlagsQuiz";
 
 function App() {
+  const [homeScreen, setHomeScreen] = useState(false);
+  const [capitalsScreen, setCapitalsScreen] = useState(true);
+  const [flagsScreen, setFlagsScreen] = useState(false);
+
   return (
     <div className="container w-4/5 m-auto">
-      <Home />
+      {homeScreen && (
+        <Home
+          setHomeScreen={setHomeScreen}
+          setCapitalsScreen={setCapitalsScreen}
+          setFlagsScreen={setFlagsScreen}
+        />
+      )}
+      {capitalsScreen && <CapitalsQuiz />}
+      {flagsScreen && <FlagsQuiz />}
     </div>
   );
 }
