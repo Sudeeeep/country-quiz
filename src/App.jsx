61921b6a4fef84f182c4ad21080e887f0cfcb8a1
Sudeeeep <sudeepn15@gmail.com>
2023-06-16 22:56:52 +0530

    feat: add toggle quiz functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { Home } from "./components/Home";
+import { CapitalsQuiz } from "./components/CapitalsQuiz";
+import { FlagsQuiz } from "./components/FlagsQuiz";
 
 function App() {
+  const [homeScreen, setHomeScreen] = useState(true);
+  const [capitalsScreen, setCapitalsScreen] = useState(false);
+  const [flagsScreen, setFlagsScreen] = useState(false);
+
   return (
     <div className="container w-4/5 m-auto">
-      <Home />
+      {homeScreen && (
+        <Home
+          setHomeScreen={setHomeScreen}
+          setCapitalsScreen={setCapitalsScreen}
+          setFlagsScreen={setFlagsScreen}
+        />
+      )}
+      {capitalsScreen && <CapitalsQuiz />}
+      {flagsScreen && <FlagsQuiz />}
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
 import countryIcon from "../assets/countries.png";
 import flagIcon from "../assets/red-flag.png";
+import propTypes from "prop-types";
 
-export const Home = () => {
+export const Home = ({ setHomeScreen, setCapitalsScreen, setFlagsScreen }) => {
+  const toggleCapitalsQuiz = () => {
+    setHomeScreen(false);
+    setCapitalsScreen(true);
+  };
+
+  const toggleFlagsQuiz = () => {
+    setHomeScreen(false);
+    setFlagsScreen(true);
+  };
+
   return (
     <>
       <h1 className="text-center text-4xl font-bold text-[#F2F2F2] mt-10">
@@ -11,7 +22,10 @@ export const Home = () => {
         <div className="bg-white p-6 flex flex-col items-center gap-6 rounded-xl">
           <img className="w-20" src={countryIcon} alt="countries icon" />
           <h2 className="text-[#2F527B] font-bold ">GUESS THE CAPITAL</h2>
-          <button className="px-6 py-3 border-2 border-[#6066D0] transition rounded-lg hover:bg-[#F9A826] hover:border-white hover:text-white hover:scale-105">
+          <button
+            className="px-6 py-3 border-2 border-[#6066D0] transition rounded-lg hover:bg-[#F9A826] hover:border-white hover:text-white hover:scale-105"
+            onClick={toggleCapitalsQuiz}
+          >
             START QUIZ
           </button>
         </div>
@@ -22,11 +36,20 @@ export const Home = () => {
             alt="countries icon"
           />
           <h2 className="text-[#2F527B] font-bold">GUESS THE FLAG</h2>
-          <button className="px-6 py-3 border-2 border-[#6066D0] transition rounded-lg hover:bg-[#F9A826] hover:border-white hover:text-white hover:scale-105">
+          <button
+            className="px-6 py-3 border-2 border-[#6066D0] transition rounded-lg hover:bg-[#F9A826] hover:border-white hover:text-white hover:scale-105"
+            onClick={toggleFlagsQuiz}
+          >
             START QUIZ
           </button>
         </div>
       </div>
     </>
   );
+};
+
+Home.propTypes = {
+  setHomeScreen: propTypes.func,
+  setCapitalsScreen: propTypes.func,
+  setFlagsScreen: propTypes.func,
 };
